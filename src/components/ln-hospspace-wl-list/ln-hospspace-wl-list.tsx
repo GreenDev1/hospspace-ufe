c8641a7aba01c16308319dba93678df4e8d0948c
Ľubomír Novotný <91133477+GreenDev1@users.noreply.github.com>
2026-05-27 23:19:06 +0200

    test flux

--- src/components/ln-hospspace-wl-list/ln-hospspace-wl-list.tsx
+++ src/components/ln-hospspace-wl-list/ln-hospspace-wl-list.tsx
@@ -24,7 +24,7 @@ export class LnHospspaceWlList {
           estimatedDurationMinutes: 20,
           condition: 'Teploty'
       }, {
-          name: 'Ing. Ferdinand Tretys',
+          name: 'Ing. Ferdinand Tretys junguj',
           patientId: '10028',
           estimatedStart: new Date(Date.now() + 5 * 60),
           estimatedDurationMinutes: 15,
